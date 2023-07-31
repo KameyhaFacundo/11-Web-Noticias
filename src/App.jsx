f6165ef286e8247ba1046a1e008972a533f49734
Facundo Kameyha <kameyhafacundo@gmail.com>
2023-07-30 22:36:02 -0300

    Agrego footer, contenedor y los componentes a usar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
 function App() {
-
   return (
     <>
+      <Container className="my-5 mainPage"></Container>
+      <footer className="text-center bg-dark text-light py-4">
+        <a href="https://github.com/KameyhaFacundo" className="text-secondary">
+          Facundo Kameyha
+        </a>
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Titulo = () => {
+  return <div></div>;
+};
+
+export default Titulo;
